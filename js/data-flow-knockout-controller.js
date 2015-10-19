1d7c6440a6b7337a10d810936f3e85ddaf4c5bd4
Jeff Ward <jeffwardcoding@gmail.com>
2015-10-19 16:14:37 -0700

    built out main js

---
+++ js/data-flow-knockout-controller.js
@@ -0,0 +1,67 @@
+alert("Javascipt has loaded correctly!")
+
+function MyViewModel() {
+    var self = this;
+
+    //Grabs dueForSale from DOM
+    self.dueForSale = ko.observable();
+    //Grabs amountRecieved from DOM
+    self.amountReceived = ko.observable();
+
+    //Computes Change
+    self.change = ko.computed(function () {
+        var change = self.amountReceived() - self.dueForSale();
+
+        if (isNaN(change) || change < 0) return '';
+
+        return change;
+    });
+
+    self.bills = {
+        hundreds: ko.computed(function () {
+            var numOfhundred = Math.floor(self.change/100);
+            return numOfhundred;
+        }),
+        fifties: ko.computed(function () {
+            var numOfFifties = Math.floor((self.change%100) / 50);
+            return numOfFifties;
+        }),
+        twenties: ko.computed(function () {
+            var numOfTwenties = Math.floor(((self.change%100) % 50 ) / 20);
+            return numOfTwenties;
+        }),
+        tens: ko.computed(function () {
+            var numOfTens = Math.floor((((self.change%100)%50)%20)/10);
+            return numOfTens;
+        }),
+        fives: ko.computed(function () {
+            var numOfFives = Math.floor(((((self.change%100)%50)%20)%10)/5);
+            return numOfFives;
+        }),
+        ones: ko.computed(function () {
+            var numOfOnes = Math.floor((((((self.change%100)%50)%20)%10)%5)/1);
+            return numOfOnes;
+        })
+    };
+
+    self.coins = {
+        quarters: ko.computed(function () {
+            var numOfQuarters = Math.floor(((((((self.change%100)%50)%20)%10)%5)%1)/.25);
+            return numOfQuarters;
+        }),
+        dimes: ko.computed(function () {
+            var numOfDimes = Math.floor((((((((self.change%100)%50)%20)%10)%5)%1)%.25)/.10);
+            return numOfDimes;
+        }),
+        nickels: ko.computed(function () {
+            var numOfNickels = Math.floor(((((((((self.change%100)%50)%20)%10)%5)%1)%.25)%.10)/.05);
+            return numOfNickels;
+        }),
+        pennies: ko.computed(function () {
+            var numOfPennies = Math.floor((((((((((self.change%100)%50)%20)%10)%5)%1)%.25)%.10)%.05)/.01);
+            return numOfPennies;
+        })
+    };
+};
+
+ko.applyBindings(new MyViewModel());
